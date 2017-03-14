4c40645a5555b2847a6e87a3a2866fa9b6ae8eea
Kyle <kdornblaser@gmail.com>
2017-03-14 17:30:55 -0400

    Update homepage with hidden shelter lists and add javascript to show shelter lists.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,14 @@
 class HomeController < ApplicationController
 
   def index
-    @shelters_states = {}
     @states = {}
     states = State.all
 
     states.each do |state|
-      @states[:"#{state.name}"] = state.get_average_weather
-    end
-
-    states.each do |state|
-      @shelters_states[:"#{state.name}"] = Shelter.where(state: state)
+      state_info = {}
+      state_info[:average_weather] = state.get_average_weather
+      state_info[:shelters] = Shelter.where(state: state)
+      @states[:"#{state.name}"] = state_info
     end
   end
 
