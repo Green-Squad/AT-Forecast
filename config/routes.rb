850f54438ba96b5dcc043a7711073513f993cadf
Jordan Smith <jordan.r.smith@gmail.com>
2017-04-05 15:32:12 -0400

    Add about page for site, contact, and adjusted temp info. Modified some styling. Added navigation to sidebar.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root 'home#index'
   resources :shelters, only: [:show]
-  get 'nearest_shelter', to: 'home#nearest_shelter',
-    as: :nearest_shelter
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'nearest_shelter',  to: 'home#nearest_shelter',   as: :nearest_shelter
+  get 'about',            to: 'home#about',             as: :about
 end
