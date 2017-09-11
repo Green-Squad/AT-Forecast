ba5711405ad24ae4d361704bc9d848229dd452cf
Kyle <kdornblaser@gmail.com>
2017-09-11 16:56:16 -0400

    Add gem rack-cors to allow access to API with JavaScript clients.

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,12 @@ module FastAtWeather
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get]
+      end
+    end
   end
 end
